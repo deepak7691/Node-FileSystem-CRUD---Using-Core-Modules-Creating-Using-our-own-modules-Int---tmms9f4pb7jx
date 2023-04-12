74b0f9ea14395e078d88f4b84e54c860f6008b25
deepak7691 <119676343+deepak7691@users.noreply.github.com>
2023-04-12 18:19:40 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const fs = require('fs/promises')
+
+const myFileWriter = async (fileName, fileContent) => {
+	// write code here
+	// dont chnage function name
+}
+
+const myFileReader = async (fileName) => {
+	// write code here
+	// dont chnage function name
+}
+
+
+const myFileUpdater = async (fileName, fileContent) => {
+	// write code here
+	// dont chnage function name
+}
+
+const myFileDeleter = async (fileName) => {
+	// write code here
+	// dont chnage function name
+}
+
+
+
+module.exports = { myFileWriter, myFileUpdater, myFileReader, myFileDeleter }
