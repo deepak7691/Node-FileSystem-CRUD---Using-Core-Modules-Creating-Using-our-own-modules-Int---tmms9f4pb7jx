9177586cde03f7e7492b6ff672c59b58bd84b178
deepak7691 <119676343+deepak7691@users.noreply.github.com>
2023-04-12 18:21:54 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,26 +1,42 @@
-const fs = require('fs/promises')
+const fs = require('fs/promises');
 
 const myFileWriter = async (fileName, fileContent) => {
-	// write code here
-	// dont chnage function name
-}
+	try {
+		await fs.writeFile(fileName, fileContent);
+		console.log(`File '${fileName}' created successfully!`);
+	} catch (err) {
+		console.error(`Failed to create file '${fileName}': ${err.message}`);
+	}
+};
 
 const myFileReader = async (fileName) => {
-	// write code here
-	// dont chnage function name
-}
-
+	try {
+		const data = await fs.readFile(fileName, 'utf-8');
+		console.log(`Content of file '${fileName}':\n${data}`);
+		return data;
+	} catch (err) {
+		console.error(`Failed to read file '${fileName}': ${err.message}`);
+	}
+};
 
 const myFileUpdater = async (fileName, fileContent) => {
-	// write code here
-	// dont chnage function name
-}
+	try {
+		const data = await fs.readFile(fileName, 'utf-8');
+		const updatedContent = data + fileContent;
+		await fs.writeFile(fileName, updatedContent);
+		console.log(`File '${fileName}' updated successfully!`);
+	} catch (err) {
+		console.error(`Failed to update file '${fileName}': ${err.message}`);
+	}
+};
 
 const myFileDeleter = async (fileName) => {
-	// write code here
-	// dont chnage function name
-}
+	try {
+		await fs.unlink(fileName);
+		console.log(`File '${fileName}' deleted successfully!`);
+	} catch (err) {
+		console.error(`Failed to delete file '${fileName}': ${err.message}`);
+	}
+};
 
-
-
-module.exports = { myFileWriter, myFileUpdater, myFileReader, myFileDeleter }
+module.exports = { myFileWriter, myFileUpdater, myFileReader, myFileDeleter };
