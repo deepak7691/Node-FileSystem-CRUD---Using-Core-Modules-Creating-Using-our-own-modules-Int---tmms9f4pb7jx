6cf5671e502adcdc1d98c95a796bad17fda5b0cc
Deepak Kawliya <kawliya11@gmail.com>
2023-04-21 22:10:36 +0530

    deepak

--- src/index.js
+++ src/index.js
@@ -39,4 +39,58 @@ const myFileDeleter = async (fileName) => {
 	}
 };
 
+
 module.exports = { myFileWriter, myFileUpdater, myFileReader, myFileDeleter };
+
+
+// const fs = require('fs');
+
+// const myFileWriter = (fileName, fileContent, callback) => {
+// 	fs.writeFile(fileName, fileContent, (err) => {
+// 		if (err) {
+// 			callback(`Failed to create file '${fileName}': ${err.message}`);
+// 		} else {
+// 			callback(null, `File '${fileName}' created successfully!`);
+// 		}
+// 	});
+// };
+
+// const myFileReader = (fileName, callback) => {
+// 	fs.readFile(fileName, 'utf-8', (err, data) => {
+// 		if (err) {
+// 			callback(`Failed to read file '${fileName}': ${err.message}`);
+// 		} else {
+// 			console.log(`Content of file '${fileName}':\n${data}`);
+// 			callback(null, data);
+// 		}
+// 	});
+// };
+
+// const myFileUpdater = (fileName, fileContent, callback) => {
+// 	fs.readFile(fileName, 'utf-8', (err, data) => {
+// 		if (err) {
+// 			callback(`Failed to update file '${fileName}': ${err.message}`);
+// 		} else {
+// 			const updatedContent = data + fileContent;
+// 			fs.writeFile(fileName, updatedContent, (err) => {
+// 				if (err) {
+// 					callback(`Failed to update file '${fileName}': ${err.message}`);
+// 				} else {
+// 					callback(null, `File '${fileName}' updated successfully!`);
+// 				}
+// 			});
+// 		}
+// 	});
+// };
+
+// const myFileDeleter = (fileName, callback) => {
+// 	fs.unlink(fileName, (err) => {
+// 		if (err) {
+// 			callback(`Failed to delete file '${fileName}': ${err.message}`);
+// 		} else {
+// 			callback(null, `File '${fileName}' deleted successfully!`);
+// 		}
+// 	});
+// };
+
+// module.exports = { myFileWriter, myFileUpdater, myFileReader, myFileDeleter };
